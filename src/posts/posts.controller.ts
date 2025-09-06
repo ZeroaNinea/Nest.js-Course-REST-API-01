7da42293f4ab9150d3b7e00b24caeff379a07a22
Catgirl <zeroaninea@gmail.com>
2025-09-07 00:39:12 +0400

    findAll created

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { Post } from './interfaces/post.interface';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(@Inject() private readonly postsService: PostsService) {}
+
+  @Get()
+  findAll(@Query('search') search?: string): Post[] {
+    const extractAllPosts = this.postsService.findAll();
+
+    if (search) {
+      return extractAllPosts.filter((singlePost) =>
+        singlePost.title.toLowerCase().includes(search.toLowerCase()),
+      );
+    }
+
+    return extractAllPosts;
+  }
+}
