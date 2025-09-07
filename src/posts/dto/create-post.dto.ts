f8cbb963d084f71f44d32f55979f2fdb2f245861
Catgirl <zeroaninea@gmail.com>
2025-09-07 15:43:24 +0400

    isOptional added

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -5,16 +5,16 @@ export class CreatePostDto {
   @IsString({ message: 'Title must be a string.' })
   @MinLength(3, { message: 'Title must be at least 3 characters.' })
   @MaxLength(50, { message: 'Title cannot be longer than 50 characters.' })
-  title?: string;
+  title: string;
 
   @IsNotEmpty({ message: 'Content is required.' })
   @IsString({ message: 'Content must be a string.' })
   @MinLength(3, { message: 'Content must be at least 3 characters.' })
-  content?: string;
+  content: string;
 
   @IsNotEmpty({ message: 'Author is required.' })
   @IsString({ message: 'Author must be a string.' })
   @MinLength(3, { message: 'Author must be at least 3 characters.' })
   @MaxLength(25, { message: 'Author cannot be longer than 50 characters.' })
-  authorName?: string;
+  authorName: string;
 }
--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,17 +1,26 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class UpdatePostDto {
+  @IsOptional()
   @IsNotEmpty({ message: 'Title is required.' })
   @IsString({ message: 'Title must be a string.' })
   @MinLength(3, { message: 'Title must be at least 3 characters.' })
   @MaxLength(50, { message: 'Title cannot be longer than 50 characters.' })
   title?: string;
 
+  @IsOptional()
   @IsNotEmpty({ message: 'Content is required.' })
   @IsString({ message: 'Content must be a string.' })
   @MinLength(3, { message: 'Content must be at least 3 characters.' })
   content?: string;
 
+  @IsOptional()
   @IsNotEmpty({ message: 'Author is required.' })
   @IsString({ message: 'Author must be a string.' })
   @MinLength(3, { message: 'Author must be at least 3 characters.' })
