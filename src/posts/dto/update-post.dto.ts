9e8a1c80d13f1b00b34c909d6bac06f5017343aa
Catgirl <zeroaninea@gmail.com>
2025-09-07 15:38:49 +0400

    update-post.dto.ts created

---
+++ src/posts/dto/update-post.dto.ts
@@ -0,0 +1,20 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class UpdatePostDto {
+  @IsNotEmpty({ message: 'Title is required.' })
+  @IsString({ message: 'Title must be a string.' })
+  @MinLength(3, { message: 'Title must be at least 3 characters.' })
+  @MaxLength(50, { message: 'Title cannot be longer than 50 characters.' })
+  title: string;
+
+  @IsNotEmpty({ message: 'Content is required.' })
+  @IsString({ message: 'Content must be a string.' })
+  @MinLength(3, { message: 'Content must be at least 3 characters.' })
+  content: string;
+
+  @IsNotEmpty({ message: 'Author is required.' })
+  @IsString({ message: 'Author must be a string.' })
+  @MinLength(3, { message: 'Author must be at least 3 characters.' })
+  @MaxLength(25, { message: 'Author cannot be longer than 50 characters.' })
+  authorName: string;
+}
