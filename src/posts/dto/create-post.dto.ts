13ab89f8fdfc8e80c9933afd7ea48b1679a23065
Catgirl <zeroaninea@gmail.com>
2025-09-07 15:35:52 +0400

    validators added

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -6,4 +6,15 @@ export class CreatePostDto {
   @MinLength(3, { message: 'Title must be at least 3 characters.' })
   @MaxLength(50, { message: 'Title cannot be longer than 50 characters.' })
   title: string;
+
+  @IsNotEmpty({ message: 'Content is required.' })
+  @IsString({ message: 'Content must be a string.' })
+  @MinLength(3, { message: 'Content must be at least 3 characters.' })
+  content: string;
+
+  @IsNotEmpty({ message: 'Author is required.' })
+  @IsString({ message: 'Author must be a string.' })
+  @MinLength(3, { message: 'Author must be at least 3 characters.' })
+  @MaxLength(25, { message: 'Author cannot be longer than 50 characters.' })
+  authorName: string;
 }
