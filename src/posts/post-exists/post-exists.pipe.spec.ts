9a2edbb82ad60215ef6b94a05fd5216d4963cdd9
Catgirl <zeroaninea@gmail.com>
2025-09-07 17:08:30 +0400

    the PostExistsPipe created

---
+++ src/posts/post-exists/post-exists.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { PostExistsPipe } from './post-exists.pipe';
+
+describe('PostExistsPipe', () => {
+  it('should be defined', () => {
+    expect(new PostExistsPipe()).toBeDefined();
+  });
+});
---
+++ src/posts/post-exists/post-exists.pipe.ts
@@ -0,0 +1,27 @@
+import {
+  // ArgumentMetadata,
+  Inject,
+  Injectable,
+  NotFoundException,
+  PipeTransform,
+} from '@nestjs/common';
+import { PostsService } from '../posts.service';
+
+@Injectable()
+export class PostExistsPipe implements PipeTransform {
+  constructor(@Inject() private readonly postsService?: PostsService) {}
+
+  transform(
+    value: number,
+    // metadata: ArgumentMetadata
+  ) {
+    try {
+      this.postsService?.findOne(value);
+    } catch (error) {
+      console.error(error);
+      throw new NotFoundException(`Post with ID ${value} not found.`);
+    }
+
+    return value;
+  }
+}
