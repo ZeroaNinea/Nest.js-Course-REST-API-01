8dcefa0dcabe118caaf7930e3e0a4d2c04bb59db
Catgirl <zeroaninea@gmail.com>
2025-09-08 23:31:59 +0400

    post.entity.ts filled by data

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,7 +1,28 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Post {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @Column()
+  authorName: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
 }
