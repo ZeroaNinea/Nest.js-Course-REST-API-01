cdaccd0e94930bbfb1e0189c5299b2c611168f34
Catgirl <zeroaninea@gmail.com>
2025-09-09 00:16:30 +0400

    the entity imported into the posts module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { Post } from './entities/post.entity';
 
 @Module({
+  // This will make the post repository available to the PostsController.
+  imports: [TypeOrmModule.forFeature([Post])],
   controllers: [PostsController],
   providers: [PostsService],
 })
