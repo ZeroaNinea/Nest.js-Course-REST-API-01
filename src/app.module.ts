d5c5d876441a0e97f8b6251094634676989fa6f4
Catgirl <zeroaninea@gmail.com>
2025-09-08 20:18:54 +0400

    environment variables imported

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { PostsController } from './posts/posts.controller';
 import { PostsService } from './posts/posts.service';
 import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PostsModule,
+  ],
   controllers: [AppController, PostsController],
   providers: [AppService, PostsService],
 })
