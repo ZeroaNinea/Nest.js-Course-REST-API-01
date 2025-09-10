b1a617343a36c56a6211205a64d88ded6f23dfd6
Catgirl <zeroaninea@gmail.com>
2025-09-10 16:16:30 +0400

    @nestjs/passport and @nestjs/jwt added into the README.md file

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This part covers **Modules**, **Controllers**, **Services**, and using `@nestjs/
 - Working on creating **Rest APIs**.
 - **DTO**, **Pipes**, and **Validation**.
 - **TypeORM** and database integration with **PostgresSQL**.
-- **JWT**.
+- **JWT**, **`@nestjs/passport`**, and **`@nestjs/jwt`**.
 
 ## Deploy PostgresSQL
 
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User } from './entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), PassportModule],
   exports: [TypeOrmModule],
   controllers: [AuthController],
   providers: [AuthService],
