66d2be49917ca8ed331dd16436f0f4ac24ab0195
Catgirl <zeroaninea@gmail.com>
2025-09-10 16:18:35 +0400

    errors fixed in auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User } from './entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), PassportModule],
-  exports: [TypeOrmModule],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule,
+    JwtModule.register({}),
+  ],
+  exports: [TypeOrmModule, AuthService],
   controllers: [AuthController],
   providers: [AuthService],
 })
