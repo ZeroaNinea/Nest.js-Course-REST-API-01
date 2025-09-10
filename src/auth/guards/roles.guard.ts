0c5d5edf4ba32be083112d814b40b4a96946fb79
Catgirl <zeroaninea@gmail.com>
2025-09-10 20:44:08 +0400

    the canActivate method created

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,40 @@
+import {
+  Injectable,
+  CanActivate,
+  ForbiddenException,
+  ExecutionContext,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+import { User, UserRole } from '../entities/user.entity';
+import { ROLES_KEY } from '../decorators/roles.decorators';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const user: User = context.switchToHttp().getRequest();
+
+    if (!user) {
+      throw new ForbiddenException('User is not authenticated.');
+    }
+
+    const hasRequiredRole = requiredRoles.some((role) => user.role === role);
+
+    if (!hasRequiredRole) {
+      throw new ForbiddenException('User does not have the required role.');
+    }
+
+    return true;
+  }
+}
