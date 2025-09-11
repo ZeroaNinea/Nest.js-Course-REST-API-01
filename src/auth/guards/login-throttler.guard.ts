e2acd93c11c2332818ddceac3eace4b2ac8d92ca
Catgirl <zeroaninea@gmail.com>
2025-09-11 23:28:19 +0400

    cries

--- src/auth/guards/login-throttler.guard.ts
+++ src/auth/guards/login-throttler.guard.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { ThrottlerGuard } from '@nestjs/throttler';
+import { Request } from 'express';
 
 @Injectable()
-export class LoginThrottlerGuard extends ThrottlerGuard {}
+export class LoginThrottlerGuard extends ThrottlerGuard {
+  protected async getTracker(req: Record<string, any>): Promise<string> {
+    const email = req.body?.email || 'anonymous';
+    return `login-${email}`;
+  }
+}
