b4aad9dad4f11be139718223c0d1dbb4255bf4c6
Catgirl <zeroaninea@gmail.com>
2025-09-12 14:26:24 +0400

    the file refactored page and limit both are added

--- src/common/dto/pagination-query.dto.ts
+++ src/common/dto/pagination-query.dto.ts
@@ -4,6 +4,12 @@ import { IsOptional, IsInt, Min, Max } from 'class-validator';
 export class PaginationQueryDto {
   @IsOptional()
   @Type(() => Number)
+  @IsInt({ message: 'Page must be an integer.' })
+  @Min(1, { message: 'Page must be at least 1.' })
+  page?: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
   @IsInt({ message: 'Limit must be an integer.' })
   @Min(1, { message: 'Limit must be at least 1.' })
   @Max(100, { message: `Limit can't exceed 100.` })
