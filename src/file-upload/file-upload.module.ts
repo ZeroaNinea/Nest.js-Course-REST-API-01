25a571d128be5e176f73b123de53d73a8ca83c9d
Catgirl <zeroaninea@gmail.com>
2025-09-13 14:23:44 +0400

    MulterModule imported

--- src/file-upload/file-upload.module.ts
+++ src/file-upload/file-upload.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MulterModule } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
 
 import { FileUploadController } from './file-upload.controller';
 import { FileUploadService } from './file-upload.service';
@@ -8,7 +10,13 @@ import { CloudinaryModule } from './cloudinary/cloudinary.module';
 import { File } from './entities/file.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([File]), CloudinaryModule],
+  imports: [
+    TypeOrmModule.forFeature([File]),
+    CloudinaryModule,
+    MulterModule.register({
+      storage: memoryStorage(),
+    }),
+  ],
   controllers: [FileUploadController],
   providers: [FileUploadService],
 })
