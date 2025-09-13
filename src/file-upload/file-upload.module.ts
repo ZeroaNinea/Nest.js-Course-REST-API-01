a62a7f46e76e590e520e008d9096b7f3c845c5b6
Catgirl <zeroaninea@gmail.com>
2025-09-13 14:20:38 +0400

    CloudinaryModule imported into the file-upload.module.ts

--- src/file-upload/file-upload.module.ts
+++ src/file-upload/file-upload.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { FileUploadController } from './file-upload.controller';
 import { FileUploadService } from './file-upload.service';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
 
 import { File } from './entities/file.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([File])],
+  imports: [TypeOrmModule.forFeature([File]), CloudinaryModule],
   controllers: [FileUploadController],
   providers: [FileUploadService],
 })
