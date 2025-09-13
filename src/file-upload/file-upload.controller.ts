e1477eafc3c787114d8c3012a5bb70913a736235
Catgirl <zeroaninea@gmail.com>
2025-09-13 15:14:31 +0400

    uploadFile created in the controller

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -1,7 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
 import { FileUploadService } from './file-upload.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UploadFileDto } from './dto/upload-file.dto';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+
+import { User } from '../auth/entities/user.entity';
 
 @Controller('file-upload')
 export class FileUploadController {
-  constructor(private readonly fuleUploadService: FileUploadService) {}
+  constructor(private readonly fileUploadService: FileUploadService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() uploadFileDto: UploadFileDto,
+    @CurrentUser() user: User,
+  ): Promise<any> {
+    if (!file) {
+      throw new BadRequestException('File is required.');
+    }
+
+    return this.fileUploadService.uploadFile(
+      file,
+      uploadFileDto.description,
+      user,
+    );
+  }
 }
