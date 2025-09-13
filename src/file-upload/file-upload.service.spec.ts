1920237ef5a9419c8e268cb46c5075544f54d997
Catgirl <zeroaninea@gmail.com>
2025-09-13 14:35:39 +0400

    the File entity injected

--- src/file-upload/file-upload.service.spec.ts
+++ src/file-upload/file-upload.service.spec.ts
@@ -1,11 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { FileUploadService } from './file-upload.service';
+import { File } from './entities/file.entity';
 
 describe('FileUploadService', () => {
   let service: FileUploadService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          // entities: [File],
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([File]),
+      ],
       providers: [FileUploadService],
     }).compile();
 
--- src/file-upload/file-upload.service.ts
+++ src/file-upload/file-upload.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { File } from './entities/file.entity';
 
 @Injectable()
-export class FileUploadService {}
+export class FileUploadService {
+  constructor(
+    @InjectRepository(File)
+    private readonly fileRepository: Repository<File>,
+  ) {}
+}
