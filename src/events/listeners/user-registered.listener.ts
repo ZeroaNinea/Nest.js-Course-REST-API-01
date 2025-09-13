ac76729ca3e5bb2947bb35178f4df11f8d91df23
Catgirl <zeroaninea@gmail.com>
2025-09-13 18:31:04 +0400

    the emitUserRegistered method created

--- src/events/listeners/user-registered.listener.ts
+++ src/events/listeners/user-registered.listener.ts
@@ -1,7 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { EventEmitter2 } from '@nestjs/event-emitter';
+import { User } from '../../auth/entities/user.entity';
+import { UserRegisteredEvent } from '../interfaces/user-registered-event.interface';
 
 @Injectable()
 export class UserEventsService {
   constructor(private readonly eventEmitter: EventEmitter2) {}
+
+  emitUserRegistered(user: User): void {
+    const UserRegisteredEventData: UserRegisteredEvent = {
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+      },
+      timeStamp: new Date(),
+    };
+
+    this.eventEmitter.emit('user.registered', UserRegisteredEventData);
+  }
 }
