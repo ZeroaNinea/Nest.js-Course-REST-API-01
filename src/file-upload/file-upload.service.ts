e7fffdc8813b856880ca85eed3ceb194886b6d77
Catgirl <zeroaninea@gmail.com>
2025-09-13 14:56:20 +0400

    the uploadFile method created

--- src/file-upload/file-upload.service.ts
+++ src/file-upload/file-upload.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 
 import { CloudinaryService } from './cloudinary/cloudinary.service';
 import { File } from './entities/file.entity';
+import { User } from '../auth/entities/user.entity';
 
 @Injectable()
 export class FileUploadService {
@@ -12,4 +13,24 @@ export class FileUploadService {
     private readonly fileRepository: Repository<File>,
     private readonly cloudinaryService: CloudinaryService,
   ) {}
+
+  async uploadFile(
+    file: Express.Multer.File,
+    description: string | undefined,
+    user: User,
+  ): Promise<File> {
+    const cloudinaryResponse = await this.cloudinaryService.uploadFile(file);
+
+    const newlyCreatedFile = this.fileRepository.create({
+      originalName: file.originalname,
+      mimeType: file.mimetype,
+      size: file.size,
+      url: cloudinaryResponse.secure_url,
+      publicId: cloudinaryResponse.public_id,
+      description,
+      uploader: user,
+    });
+
+    return this.fileRepository.save(newlyCreatedFile);
+  }
 }
