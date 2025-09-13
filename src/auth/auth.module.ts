bda18c294c023001c1de012a1d8da4b66f9ffa1a
Catgirl <zeroaninea@gmail.com>
2025-09-13 19:27:40 +0400

    EventsModule imported into the AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,11 +8,14 @@ import { AuthService } from './auth.service';
 import { User } from './entities/user.entity';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { RolesGuard } from './guards/roles.guard';
+import { EventsModule } from '../events/events.module';
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     PassportModule,
     JwtModule.register({}),
+    EventsModule,
   ],
   exports: [TypeOrmModule, AuthService, RolesGuard],
   controllers: [AuthController],
